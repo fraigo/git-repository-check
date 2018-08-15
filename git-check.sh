9684e69d9f62a6f98c75a048e95b5ecb4d6901b0
Francisco Igor <franciscoigor@gmail.com>
2018-08-15 12:26:48 -0700

    added parameters

--- git-check.sh
+++ git-check.sh
@@ -7,33 +7,43 @@ message () {
 DIR=$(find ./ -maxdepth 1 -type d -printf "%f ")
 CURR=$(pwd)
 
+FETCH=$(echo "$1 $2 $3" | grep "\-fetch")
+INTERACTIVE=$(echo "$1 $2 $3" | grep "\-interactive")
+
 for f in $DIR
 do
 	if [ ! "%f" == "./" ]  && [ ! "%f" == "../" ]; then
 		cd $f
 		if [ -e .git ]; then
+			if [ ! "$FETCH" == "" ]; then
+				git fetch origin master > /dev/null
+			fi
 			RES=$(git status | grep "	")
 			if [ ! "$RES" == "" ]; then
 				message "**** $f : for commit **********"
 				echo "$RES"
-				echo "-------------------------------"
-				echo "Commit message [Empty to cancel]: "
-				read COMMIT_MESSAGE
-				if [ ! "$COMMIT_MESSAGE" == "" ]; then
-					git add -A
-					git commit -m "$COMMIT_MESSAGE"
+				if [ ! "$INTERACTIVE" == "" ]; then
+					echo "-------------------------------"
+					echo "Commit message [Empty to cancel]: "
+					read COMMIT_MESSAGE
+					if [ ! "$COMMIT_MESSAGE" == "" ]; then
+						git add -A
+						git commit -m "$COMMIT_MESSAGE"
+					fi
 				fi
 				sleep 1
 			fi
 			RES1=$(git status | grep "Your branch is ahead")
-			if [ ! "$RES1" == "" ]; then
+			if [ ! "$RES1" == "" ] ; then
 				message "**** $f : for push   **********"
 				echo "$RES1"
-				echo "-------------------------------"
-				echo "Push to origin [ Empty to cancel ] ? :"
-				read PUSH_ORIGIN
-				if [ ! "$PUSH_ORIGIN" == "" ]; then
-					git push origin $PUSH_ORIGIN
+				if [ ! "$INTERACTIVE" == "" ]; then
+					echo "-------------------------------"
+					echo "Push to origin [ Empty to cancel ] ? :"
+					read PUSH_ORIGIN
+					if [ ! "$PUSH_ORIGIN" == "" ]&& [ ! "$INTERACTIVE" == "" ]; then
+						git push origin $PUSH_ORIGIN
+					fi
 				fi
 				sleep 1
 			fi
